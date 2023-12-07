7e8f5a92d3ab7d1c8042dd7c6d5d001e7cef9990
AthiraAduvattu <119652267+AthiraAduvattu@users.noreply.github.com>
2023-12-07 20:47:03 +0530

    bizcard1.py

--- bizcard1.py
+++ bizcard1.py
@@ -49,7 +49,7 @@ pip install streamlit-option-menu
 # selected = option_menu(None, ["Home","Upload & Extract","Modify"],default_index=0,
 #                        orientation="horizontal")
 # if selected=='Home':
-#     st.write("Bizcard Extraction is a Python application built with Streamlit, EasyOCR, OpenCV, regex function, and MySQL database. It allows users to extract information from business cards and store it in a MySQL database for further analysis. The main purpose of Bizcard is to automate the process of extracting key details from business card images, such as the name, designation, company, contact information, and other relevant data. By leveraging the power of OCR (Optical Character Recognition) provided by EasyOCR, Bizcard is able to extract text from the images.")
+#     st.write("Bizcard Extraction is a Python application built with Streamlit, EasyOCR, regex function, and MySQL database. It allows users to extract information from business cards and store it in a MySQL database for further analysis. The main purpose of Bizcard is to automate the process of extracting key details from business card images, such as the name, designation, company, contact information, and other relevant data. By leveraging the power of OCR (Optical Character Recognition) provided by EasyOCR, Bizcard is able to extract text from the images.")
 # elif selected=='Upload & Extract':
 #     st.write("Welcome")
 # 
